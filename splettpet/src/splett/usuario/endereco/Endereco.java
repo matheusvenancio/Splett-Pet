34e590b7c0ccb597a569c5c41556f974e586b66a
bernachar <bernardo.actor@gmail.com>
2015-10-17 16:27:22 -0300

    toString do Endereço tá certo.

--- splettpet/src/splett/usuario/endereco/Endereco.java
+++ splettpet/src/splett/usuario/endereco/Endereco.java
@@ -10,75 +10,81 @@ import javax.persistence.Table;
 @Table(name = "tbEndereco")
 public class Endereco {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	private int numero;
-	private String logradouro, cep, bairro, complemento, uf, cidade;
+    private int numero;
+    private String logradouro, cep, bairro, complemento, uf, cidade;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+	return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+	this.id = id;
+    }
 
-	public int getNumero() {
-		return numero;
-	}
+    public int getNumero() {
+	return numero;
+    }
 
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
+    public void setNumero(int numero) {
+	this.numero = numero;
+    }
 
-	public String getLogradouro() {
-		return logradouro;
-	}
+    public String getLogradouro() {
+	return logradouro;
+    }
 
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
+    public void setLogradouro(String logradouro) {
+	this.logradouro = logradouro;
+    }
 
-	public String getCep() {
-		return cep;
-	}
+    public String getCep() {
+	return cep;
+    }
 
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
+    public void setCep(String cep) {
+	this.cep = cep;
+    }
 
-	public String getBairro() {
-		return bairro;
-	}
+    public String getBairro() {
+	return bairro;
+    }
 
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
+    public void setBairro(String bairro) {
+	this.bairro = bairro;
+    }
 
-	public String getComplemento() {
-		return complemento;
-	}
+    public String getComplemento() {
+	return (complemento == null) ? complemento : "";
+    }
 
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
+    public void setComplemento(String complemento) {
+	this.complemento = complemento;
+    }
 
-	public String getUf() {
-		return uf;
-	}
+    public String getUf() {
+	return uf;
+    }
 
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
+    public void setUf(String uf) {
+	this.uf = uf;
+    }
 
-	public String getCidade() {
-		return cidade;
-	}
+    public String getCidade() {
+	return cidade;
+    }
 
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
+    public void setCidade(String cidade) {
+	this.cidade = cidade;
+    }
+
+    public String toString() {
+	return logradouro + ", " + numero
+		+ ((complemento == null) ? ", " : (", " + complemento + ", ")) + bairro + ", "
+		+ cidade + ".";
+    }
 
 }
